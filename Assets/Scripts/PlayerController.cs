815966fbc7c9344eda98a158fc7bbb61354eb1c4
maria.hernandez <maria.hernandez@loopup.com>
2017-05-13 21:50:00 -0700

    move player in horizontal axis

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,13 +4,27 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
 
+	public float moveSpeed;
+
+	private Rigidbody2D rigidBody;
+
 	// Use this for initialization
 	void Start () {
-		
+		rigidBody = GetComponent<Rigidbody2D>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		Vector3 velocity;
+		if (Input.GetAxisRaw("Horizontal") > 0f) {
+			velocity = new Vector3(moveSpeed, rigidBody.velocity.y, 0f);
+			rigidBody.velocity = velocity; 
+		} else if (Input.GetAxisRaw("Horizontal") < 0f) {
+			velocity = new Vector3(-moveSpeed, rigidBody.velocity.y, 0f);
+			rigidBody.velocity = velocity; 
+		} else {
+			velocity = new Vector3(0f, rigidBody.velocity.y, 0f);
+			rigidBody.velocity = velocity;
+		}
 	}
 }
