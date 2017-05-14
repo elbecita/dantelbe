bf14344aa1e674fd93c9c5d237595283c513349b
maria.hernandez <maria.hernandez@loopup.com>
2017-05-14 16:35:44 -0700

    player jump

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,6 +5,13 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour {
 
 	public float moveSpeed;
+	public float jumpSpeed;
+
+	// Variables to check that the user is on the ground
+	public Transform groundCheck;
+	public float groundCheckRadius;
+	public LayerMask whatIsGround;
+	public bool isGrounded;
 
 	private Rigidbody2D rigidBody;
 
@@ -15,16 +22,27 @@ public class PlayerController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		Vector3 velocity;
+
+		// OverlapCircle checks if a collider falls within a circular area (in this case groundCheck
+		// is at the feet of the Player)
+		isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, whatIsGround);
+
+		Vector3 newVelocity;
 		if (Input.GetAxisRaw("Horizontal") > 0f) {
-			velocity = new Vector3(moveSpeed, rigidBody.velocity.y, 0f);
-			rigidBody.velocity = velocity; 
+			newVelocity = new Vector3(moveSpeed, rigidBody.velocity.y, 0f);
+			rigidBody.velocity = newVelocity; 
 		} else if (Input.GetAxisRaw("Horizontal") < 0f) {
-			velocity = new Vector3(-moveSpeed, rigidBody.velocity.y, 0f);
-			rigidBody.velocity = velocity; 
+			newVelocity = new Vector3(-moveSpeed, rigidBody.velocity.y, 0f);
+			rigidBody.velocity = newVelocity; 
 		} else {
-			velocity = new Vector3(0f, rigidBody.velocity.y, 0f);
-			rigidBody.velocity = velocity;
+			newVelocity = new Vector3(0f, rigidBody.velocity.y, 0f);
+			rigidBody.velocity = newVelocity;
+		}
+
+		// Only jump if the Player is on the ground
+		if (Input.GetButtonDown("Jump") && isGrounded) {
+			newVelocity = new Vector3(rigidBody.velocity.x, jumpSpeed, 0f);
+			rigidBody.velocity = newVelocity;
 		}
 	}
 }
