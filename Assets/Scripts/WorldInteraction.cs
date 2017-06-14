54194a6d3046922c926c7604f798ebaa0479e46e
maria.hernandez <maria.hernandez@loopup.com>
2017-06-13 21:13:27 -0700

    system to detect interactions in the world init

---
+++ Assets/Scripts/WorldInteraction.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorldInteraction : MonoBehaviour {
+
+	void Update () {
+		// IsPointerOverGameObject used to identify if we are clicking in the UI
+		if (Input.GetMouseButtonDown(0) && !UnityEngine.EventSystems.EventSystem.current.IsPointerOverGameObject()) {
+			GetInteraction();
+		}
+	}
+
+	void GetInteraction() {
+		// Detect if an object has been hit
+		Vector2 worldPoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+		RaycastHit2D hit = Physics2D.Raycast(worldPoint, Vector2.zero);
+		if (hit.collider != null) {
+			GameObject interactedObject = hit.collider.gameObject;
+			if (interactedObject.tag == "Interactable") {
+				print("Interactable object clicked! Name: " + interactedObject.name);
+			}
+		}
+	}
+}
